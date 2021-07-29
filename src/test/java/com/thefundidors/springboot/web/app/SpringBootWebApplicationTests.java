aec644a737a8d79d12698df8a8bbe5e0fc426a49
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-07-29 19:14:27 +0200

    Inicio curso Spring Boot, creamos el primer controlador y la primera vista

---
+++ src/test/java/com/thefundidors/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefundidors.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
