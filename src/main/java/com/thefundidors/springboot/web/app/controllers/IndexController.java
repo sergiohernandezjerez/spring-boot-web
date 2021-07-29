600c3175fb1bdc9b3e9962708a8c6bb524722497
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-07-29 19:49:04 +0200

    diferentes maneras de pasar datos a la vista. Model, ModelMap, Map y ModelAndView

--- src/main/java/com/thefundidors/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/thefundidors/springboot/web/app/controllers/IndexController.java
@@ -1,15 +1,39 @@
 package com.thefundidors.springboot.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping({"/index", "/", "/home"})
-	public String index() {
-		
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Framework");
 		return "index";
+	}
+	
+	@GetMapping("/indexModelMap")
+	public String index(ModelMap model) {
+		model.addAttribute("titulo", "Hola Spring Framework con ModelMap");
+		return "indexModelMap";
+	}
+	
+	@GetMapping("/indexMap")
+	public String indexMap(Map<String, Object> map) {
+		map.put("titulo", "Hola Spring Framework Map");
+		return "indexMap";
+	}
+	
+	@GetMapping("/indexModelAndView")
+	public ModelAndView indexModelAndView(ModelAndView mv) {
+		mv.addObject("titulo", "Hola Spring Framework con modelandview");
+		mv.setViewName("indexModelAndView");
+		return mv;
 	}
 
 }
