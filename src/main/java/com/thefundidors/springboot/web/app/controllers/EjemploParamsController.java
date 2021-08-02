ef8c3f3c2c815186573dfd5a8e9a4666b540dc16
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-08-02 18:31:36 +0200

    Enviar varios valores con RequestParam

--- src/main/java/com/thefundidors/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/thefundidors/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.thefundidors.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,30 @@ public class EjemploParamsController {
 	public String index() {
 		
 		return "params/index";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el número es: '" + numero + "'");
+		
+		return "params/ver";
+		
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		} catch (NumberFormatException e) {
+			numero = 0;
+		}
+		
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el número es: '" + numero + "'");
+		
+		return "params/ver";
+		
 	}
 	
 
